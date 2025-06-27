1cd6f746307830efbb8e534d84d89ca721fbeafa
Eden1811 <viocretu321@gmail.com>
2025-06-27 17:29:27 +0300

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.FullSerializer;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hello, world!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+}
